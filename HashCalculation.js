23e9837cb3a703c79d99359fc518b205fe524c54
Sebastian Guzman <bitas200225@gmail.com>
2021-10-11 22:10:03 -0500

    Actualizacion de HashCalculation

--- HashCalculation.js
+++ HashCalculation.js
@@ -1,3 +1,30 @@
 var sha256 = require('js-sha256');
 
-console.log(sha256('Message to hash'))
+console.log(sha256('Message to hash'));
+
+const prototypeHashes = {
+    1: '0',
+    2: '00',
+    3: '000',
+    4: '0000',
+    5: '00000',
+    6: '000000',
+    7: '0000000',
+    8: '00000000',
+    9: '000000000',
+    10: '0000000000',
+}
+
+const findHash = number => {
+    var flag = true;
+    
+    while(flag){
+        var hash = '0000sdasd223bxsdf24ff';
+        hash.substring(0, number) === prototypeHashes[number] ? 
+            flag = false : 
+            'create a new hash';
+    }
+    return hash;
+}
+
+console.log(findHash(5));
