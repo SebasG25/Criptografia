be7f6c79bab18f7b4eaedc5a4838de3eb1a3d634
Sebastian Guzman <bitas200225@gmail.com>
2021-10-11 23:19:07 -0500

    HashCalculation completo

--- HashCalculation.js
+++ HashCalculation.js
@@ -1,7 +1,5 @@
 var sha256 = require('js-sha256');
 
-console.log(sha256('Message to hash'));
-
 const prototypeHashes = {
     1: '0',
     2: '00',
@@ -17,14 +15,27 @@ const prototypeHashes = {
 
 const findHash = number => {
     var flag = true;
+
+    var hashObj = {
+        hash: '',
+        iterationCounter: 0,
+        executionTime: 0,
+    }
+
+    var start = Date.now()
     
     while(flag){
-        var hash = '0000sdasd223bxsdf24ff';
-        hash.substring(0, number) === prototypeHashes[number] ? 
-            flag = false : 
-            'create a new hash';
+        hashObj.hash = sha256(Math.random() + '');
+        if (hashObj.hash.substring(0, number) === prototypeHashes[number]) flag = false;
+        hashObj.iterationCounter++;
     }
-    return hash;
+
+    var end = Date.now()
+
+    hashObj.executionTime = end - start;
+
+    return hashObj;
 }
 
-console.log(findHash(5));
+
+console.log(findHash(2)); //Inserte un número para encontrar el hash
